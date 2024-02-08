ae920344bac4e256a9207a6e4052627011a54219
Allen Kinzalow <allen.kinzalow@gmail.com>
2024-02-07 21:43:45 -0500

    Add MediaWikiTable options (#7)
    * beginning of table improvements
    
    * table, row, and cell options
    
    * Fix last remaining spacing issue and add changesets

---
+++ .changeset/smart-bananas-build.md
@@ -0,0 +1,5 @@
+---
+"@osrs-wiki/mediawiki-builder": minor
+---
+
+Add support for minimal table formatting in MediaWikiTable
--- src/contents/table.ts
+++ src/contents/table.ts
@@ -1,41 +1,84 @@
 import MediaWikiContent from "../content";
+import { toKeyValueString } from "../utils/objects";
 
-export type MediaWikiTableRow = MediaWikiContent[][];
+export type MediaWikiTableParams = {
+  caption?: string;
+  rows: MediaWikiTableRow[];
+  options?: MediaWikiTableOptions;
+};
+
+export type MediaWikiTableRow = {
+  cells: MediaWikiTableCell[];
+  header?: boolean;
+  minimal?: boolean;
+  options?: MediaWikiTableRowOptions;
+};
+
+export type MediaWikiTableCell = {
+  content: MediaWikiContent[];
+  options?: MediaWikiTableCellOptions;
+};
 
 export type MediaWikiTableOptions = {
-  caption?: string;
-  center?: boolean;
-  headers?: string[];
+  class?: string;
+  style?: string;
+};
+
+export type MediaWikiTableRowOptions = {
+  class?: string;
+  style?: string;
+};
+
+export type MediaWikiTableCellOptions = {
+  class?: string;
+  colspan?: number;
+  rowspan?: number;
+  style?: string;
 };
 
 export class MediaWikiTable extends MediaWikiContent {
+  caption?: string;
   options?: MediaWikiTableOptions;
   rows: MediaWikiTableRow[];
 
-  constructor(rows: MediaWikiTableRow[], options?: MediaWikiTableOptions) {
+  constructor(params: MediaWikiTableParams) {
     super();
-    this.rows = rows;
-    this.options = options;
+    this.caption = params.caption;
+    this.rows = params.rows;
+    this.options = params.options;
   }
 
   build() {
-    return `{| class="wikitable"${
-      this.options?.center ? ' style="text-align: center;"' : ""
-    }\n${this.options?.caption ? `|+${this.options.caption}\n` : ""}${
-      this.options?.headers
-        ? `|-\n` +
-          this.options?.headers.map((header) => `!${header}\n`).join("")
-        : ""
+    const tableOptions = this.options
+      ? " " + toKeyValueString<MediaWikiTableOptions>(this.options)
+      : "";
+    return `{|${tableOptions}\n${
+      this.caption ? `|+${this.caption}\n` : ""
     }${this.rows
       .map(
         (row) =>
-          `|-\n` +
-          row
+          `|-${
+            row.options
+              ? " " + toKeyValueString<MediaWikiTableRowOptions>(row.options)
+              : ""
+          }\n` +
+          row.cells
             .map(
-              (columns) =>
-                `|` + columns.map((column) => column.build()).join("") + "\n"
+              (cell, cellIndex) =>
+                (row.header ? `!` : `|`).repeat(
+                  row.minimal && cellIndex > 0 ? 2 : 1
+                ) +
+                " " +
+                (cell.options
+                  ? `${toKeyValueString<MediaWikiTableCellOptions>(
+                      cell.options
+                    )} | `
+                  : "") +
+                cell.content.map((content) => content.build()).join("") +
+                (row.minimal ? "" : "\n")
             )
-            .join("")
+            .join(row.minimal ? " " : "") +
+          (row.minimal ? "\n" : "")
       )
       .join("")}|}`;
   }
